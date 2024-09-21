8545b2b733042580524e61d87e75ec1eaf9f3b61
Danish <akhtardanish72@gmail.com>
2024-09-21 19:29:28 +0530

    update style

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import './/Owner/Admin.css'
 
-
 import './Style/navbarmain.css'
 import './Style/careerpage.css'
 import './Style/navbar2career.css'
